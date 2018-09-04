8c315913f67ef2f894dd6399f101934db0497f74
Spencer Olson <spence1818@gmail.com>
2018-09-03 21:24:46 -0500

    basic crud app with google oauth set up

--- web/router.ex
+++ web/router.ex
@@ -7,6 +7,7 @@ defmodule Discuss.Router do
     plug :fetch_flash
     plug :protect_from_forgery
     plug :put_secure_browser_headers
+    plug Discuss.Plugs.SetUser
   end
 
   pipeline :api do
@@ -19,6 +20,17 @@ defmodule Discuss.Router do
     get "/", TopicController, :index
     get "/topics/new", TopicController, :new
     post "/topics", TopicController, :create
+    get "/topics/:id/edit", TopicController, :edit
+    put "/topics/:id/update", TopicController, :update
+    delete "/topics/:id/delete", TopicController, :delete
+  end
+
+  scope "/auth", Discuss do
+    pipe_through :browser
+
+    get "/signout", AuthController, :signout
+    get "/:provider", AuthController, :request
+    get "/:provider/callback", AuthController, :callback
   end
 
   # Other scopes may use custom stacks.
